94abb74752fd9754cab1d285bfe9a875bcc3cad9
Kinyaev Ilfat <kinyaevilfat@gmail.com>
2018-10-01 14:21:00 +0000

    fixed imports and config

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -13,11 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
-from django.urls import include
-
-from ask.qa import views
 
 # urlpatterns = [
 # 	url(r'^admin/', admin.site.urls),
